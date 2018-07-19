397943b2033ba4a8be7c3364f06e88e9f15bbd1b
d.fetinin <denis.fetinin@tionix.ru>
2018-07-19 18:39:11 +0300

    work in progress

--- snippets.py
+++ snippets.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python2.7
+from __future__ import print_function
+
 
 def calc_idx(key, n):
     i = hash(key) % n
@@ -6,6 +8,6 @@ def calc_idx(key, n):
     perturb = hash(key)
     while True:
         i = (5 * i + perturb + 1) % n
-        print "Perturb is", perturb
+        print("Perturb is", perturb)
         perturb >>= 5
         yield i
