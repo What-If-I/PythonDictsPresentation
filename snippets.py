6624a330b39d556faf10288a71224990e4e5a040
d.fetinin <denis.fetinin@tionix.ru>
2018-07-31 11:19:45 +0300

    Create html for presentation

--- snippets.py
+++ snippets.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 
 
-def calc_idx(key, n):
+def calc_idx_v_perturb(key, n):
     i = hash(key) % n
     yield i
     perturb = hash(key)
@@ -11,3 +11,15 @@ def calc_idx(key, n):
         print("Perturb is", perturb)
         perturb >>= 5
         yield i
+
+
+def calc_idx(key, n):
+    i = hash(key) % n
+    yield i
+    while True:
+        i = (5 * i + 1) % n
+        yield i
+
+
+def calc_hash_mod(key, n):
+    return hash(key) % n
