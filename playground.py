74440f9a1b39013087bd3df10722ac888f17ac90
d.fetinin <denis.fetinin@tionix.ru>
2018-08-07 15:24:10 +0300

    Update benchmarks

--- playground.py
+++ playground.py
@@ -1,7 +1,30 @@
-class A:
-    a = 'a'
-    b = 'b'
-    c = 'c'
+# class ClassNoInit(object):
+#     a = 'a'
+#     b = 'b'
+#     c = 'c'
+
+try:
+    range = xrange
+except NameError:
+    pass
 
 
-# [A() for _ in range(10000000)]
+class Foo(object):
+
+    def __init__(self, foo, bar, baz):
+        self.foo = foo
+        self.bar = bar
+        self.baz = baz
+#
+#
+# class ClassInitSlots(object):
+#     __slots__ = ['a', 'b', 'c']
+#
+#     def __init__(self, a, b, c):
+#         self.a = a
+#         self.b = b
+#         self.c = c
+
+
+# [ClassNoInit() for _ in xrange(10000000)]
+[Foo('a', 'b', 'c') for _ in range(10000000)]
